dc2b949c31dd4a40559637c8c36edbf8441349e5
uwasevicky <uwasevicky@gmail.com>
2019-10-07 18:09:13 +0200

    Correcting

--- Homes/models.py
+++ Homes/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class home(models.Model):
-	Nid =models.IntegerField(max_length=50)
+class Home(models.Model):
+	Nid =models.IntegerField()
 	DateOfBirth=models.DateField()
 	FirstName=models.CharField(max_length=250)
 	LastName=models.CharField(max_length=250)
@@ -13,36 +13,41 @@ class home(models.Model):
 	Phone2=models.IntegerField()
 	Email=models.EmailField(max_length=250)
 	Village=models.CharField(max_length=50)
-class= notification(models.Model):
+class Notification(models.Model):
 	Phone = models.IntegerField()
 	text = models.CharField(max_length=500)
 	Type = models.CharField(max_length=250)
 	Date = models.DateField()
 	Time = models.TimeField()
-class transfers (models.Model): 
-	homeid = models.ForeignKey(home, on_delete=models.CASCADE)
+class Transfers (models.Model): 
+	homeid = models.ForeignKey(Home, on_delete=models.CASCADE)
 	From  =models.CharField(max_length=10)
 	To =models.CharField(max_length=10)
 	Date =models.DateField(max_length=10)
 	Time = models.TimeField(max_length=10)
 	status =models.CharField(max_length=10)
+
+class Term(models.Model):
+	fees = models.IntegerField()
+	status = models.CharField(max_length=10)
+	Year = models.IntegerField()
+
+class Transaction(models.Model):
+	fees = models.IntegerField()
+	termN0 =models.ForeignKey(Term, on_delete = models.CASCADE)
+	date =models.DateField()
+	time =models.TimeField()
+
 class Payment (models.Model):
 	home = models.ForeignKey(Home, on_delete = models.CASCADE)
 	fees = models.IntegerField()
 	description = models.TextField()
 	date = models.DateField()
 	time = models.TimeField()
-	paymentMethod = models.CharField()
-	transctionNo = models.ForeignKey(Transction, on_delete = models.CASCADE)
-class Transction(models.Model):
-	fees = models.IntegerField()
-	termN0 =models.ForeignKey(Term, on_delete = models.CASCADE)
-	date =models.DateField()
-	time =models.TimeField()
-class Term(models.Model):
-	fees = models.IntegerField()
-	status = models.CharField(max_length=10)
-	Year = models.IntegerField()
+	paymentMethod = models.CharField(max_length=200)
+	transactionNo = models.ForeignKey(Transaction, on_delete = models.CASCADE)
+
+
 
 		
 
